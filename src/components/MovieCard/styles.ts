ba7a0c49103f6de0613396cdd4244a3022791ac2
lazarok09 <lazarok09@gmail.com>
2024-04-14 12:19:11 -0300

    style(MovieCard): adjust the font size of the movie card

--- src/components/MovieCard/styles.ts
+++ src/components/MovieCard/styles.ts
@@ -4,29 +4,43 @@ import { CustomButton } from "../CustomButton/styles";
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    align-items: center;
+
     color: red;
     width: 100%;
     padding: 16px;
     max-width: 338.67px;
     height: auto;
     max-height: 324px;
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    align-items: center;
 
     background: ${theme.colors.lightBackground};
-    display ${CustomButton} {
+
+    ${CustomButton} {
       text-transform: uppercase;
       font-weight: 700;
       line-height: 16.34px;
-      font-size: 12px;
+      font-size: 0.8rem;
 
       > span {
         height: 100%;
         display: flex;
         gap: 3.4px;
       }
+    }
+    > p {
+      font-weight: 700;
+      font-size: 1rem;
+      line-height: 21.79px;
+      text-align: center;
+      color: ${theme.colors.darkText};
+    }
+    > h3 {
+      color: ${theme.colors.darkText};
+      font-size: 0.8rem;
+      line-height: 16.34px;
     }
   `}
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,7 @@ export const theme = {
   colors: {
     white: "#FFFFFF",
     darkText: "#2F2E41",
+    darkTextTitle: "#333333",
     darkBackground: "#2F2E41",
     mobileHeader: "#1D1D2B",
     lightBlue: "#009EDD",
