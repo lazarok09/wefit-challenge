4ac316d252f88d0462e46f8ad305289bc65b309a
lazarok09 <lazarok09@gmail.com>
2024-04-15 00:18:54 -0300

    refactor(Loading): adjust loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 const LOADING_ICON = "/icons/loading.svg";
+import * as Styled from "./styles";
 export const Loading = () => {
-  return "loading....";
-  return <Image src={LOADING_ICON} height={30} width={30} alt="carregando" />;
+  return (
+    <Styled.LoadingContainer>
+      <Image src={LOADING_ICON} height={55.41} width={55.41} alt="carregando" />
+    </Styled.LoadingContainer>
+  );
 };
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
+`;
